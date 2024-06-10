a9032f694c1d22ff6b34c48208397883f8d26643
jaehyeok <charbori@github.com>
2024-06-11 02:00:04 +0900

    add rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,37 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/video-storage/:path*",
+                destination: "http://content1.couhensoft.com:9000/:path*",
+            },
+            {
+                source: "/:path*",
+                destination: "https://api.couhensoft.com/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
